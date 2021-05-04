b59367d8d6348cfe8195e08e7e4baa624fdbb75a
Muhammad Waqar <Muhammad.waqar@tiybr.com>
2021-05-04 13:41:45 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Carousel
+        infiniteLoop={true}
+        showThumbs={false}
+        autoPlay={true}
+        stopOnHover={false}
+        dynamicHeight={false}
+      >
+        <div>
+          <img alt="heading theme 1" src={img1} />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img alt="heading theme 2" src={img2} />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img alt="heading theme 3" src={img3} />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
     </div>
   );
 }
